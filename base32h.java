57c9e9670fa3b09c244e3beefe78b64f8c362a22
JamesFloyd-Pen <Commanderpip@gmail.com>
2021-01-18 13:59:46 -0500

    TODO Commit
    A list alongside the base functions.
    Do Pseudocode next

---
+++ base32h.java
@@ -0,0 +1,50 @@
+public class base32h {
+
+    int encodeDecimal(int n)
+    {
+        int answer = 0;
+
+        return answer;
+    }
+
+
+    int encodeBinary(int n)
+    {
+        int i = 0;
+
+        return i;
+    }
+
+    int decodeDecimal(int n)
+    {
+
+        return n;
+    }
+
+    int decodeBinary(int n)
+    {
+
+        return n;
+    }
+
+
+
+    static void testMe()
+    {
+        System.out.println("Hello World!");
+
+    }
+
+    public static void main(String[] args) {
+        /* TODO code application logic here
+
+            1. Create the Encoder for Decimal first
+            2. Create the decoder for Decimal Second
+            3. Test both Hell0World, alongside test cases to pass the first part.
+            4. Build both the binary for encoder
+            5. Build the binary decoder
+            6. Test test for both
+        */
+        base32h.testMe();
+    }
+}
